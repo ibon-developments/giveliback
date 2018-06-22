ec3f951b47a4c47072512c29eac0625ad0bdf8a1
Steampunk1453 <dasies11@gmail.com>
2018-06-22 21:38:47 +0200

    add rest service

---
+++ src/app/core/services/data.service.ts
@@ -0,0 +1,38 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+
+@Injectable()
+export class DataService {
+
+  counter: Observable<number>;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+
+  get(url: string): Observable<any>  {
+    return this.http.get(url);
+  }
+
+  post(url: string, data: any): Observable<any> {
+    const json = JSON.stringify(data);
+    return this.http.post(url, json, httpOptions)
+  }
+
+  put(url: string, data: any): Observable<any> {
+    const json = JSON.stringify(data);
+    return this.http.put(url + data.id, json, httpOptions);
+  }
+
+  delete(url: string, data: any): Observable<any> {
+    return this.http.delete(url + data, httpOptions);
+  }
+
+}
