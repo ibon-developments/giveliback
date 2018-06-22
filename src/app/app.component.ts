abc3cc4f26ed8b93bde16a30d72e3a6bf92978c8
Steampunk1453 <dasies11@gmail.com>
2018-06-22 21:39:28 +0200

    add calls to data service with get

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DataService} from './core/services/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor(private _dataService: DataService) {
+  }
+
+  ngOnInit(): void {
+    this._dataService.get('http://127.0.0.1:8888/get_books_by_owner').subscribe((data) => {
+      if (data) {
+        console.log(data);
+      }
+    });
+    this._dataService.get('http://127.0.0.1:8888/create_book?book_name=test&isbn=981299902').subscribe((data) => {
+      if (data) {
+        console.log(data);
+      }
+    });
+  }
 }
