7b7ef300ae2ed6e751348c1f27ea057b10ca7cb1
Steampunk1453 <dasies11@gmail.com>
2018-08-26 00:46:37 +0200

    add new modules for use reactive forms

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BooksComponent } from './books.component';
 import {BooksRoutingModule} from './books-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    BooksRoutingModule
+    BooksRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [
-    BooksComponent
+    BooksComponent,
+    FormsModule
   ],
   declarations: [BooksComponent]
 })
