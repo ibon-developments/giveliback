13a34f465d68fe2a142d9861a5c314c84c20502f
Steampunk1453 <dasies11@gmail.com>
2018-08-27 21:24:18 +0200

    remove not used method

--- src/app/books/services/contracts.service.ts
+++ src/app/books/services/contracts.service.ts
@@ -13,13 +13,4 @@ export class ContractsService {
   compileContract(): Observable<any> {
     return this.dataService.get('http://127.0.0.1:8888/compile_contracts');
   }
-  instanceContract(contractAddress: string) {
-    let params = new HttpParams();
-    params = params.append('contract_address', contractAddress);
-    this.dataService.getWithParameters('http://127.0.0.1:8888/manual_instance', params).subscribe((data) => {
-      if (data) {
-        console.log(data);
-      }
-    });
-  }
 }
